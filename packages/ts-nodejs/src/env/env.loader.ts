a3ac0cd2e333ced0b09a0fcce26193ca68ec2a54
Joao Franco <joao.franco@jsfsi.com>
2025-07-07 22:12:53 +0100

    Fix dirname usage

--- packages/ts-nodejs/src/env/env.loader.ts
+++ packages/ts-nodejs/src/env/env.loader.ts
@@ -1,6 +1,10 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 import * as dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 type LoadEnvConfigOptions = {
   env?: string;
