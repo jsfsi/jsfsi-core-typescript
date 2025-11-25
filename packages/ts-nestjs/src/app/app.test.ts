927d0336406ad89d09337a3a4a5f75abdc68e27b
Joao Franco <joao.franco@jsfsi.com>
2025-11-25 19:27:36 +0000

    Added tests for logger usage in nestjs

--- packages/ts-nestjs/src/app/app.test.ts
+++ packages/ts-nestjs/src/app/app.test.ts
@@ -1,15 +1,18 @@
 import { MockLogger } from '@jsfsi-core/ts-nodejs';
-import { BadRequestException, Controller, Get, Module } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Logger, Module } from '@nestjs/common';
 import request from 'supertest';
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 
 import { appConfigModuleSetup } from '../configuration/AppConfigurationService';
 import { createTestingApp } from '../test/testing-app';
 
 @Controller('test')
 class TestController {
+  private logger = new Logger(TestController.name);
+
   @Get()
   getHello(): { message: string } {
+    this.logger.log('some testing log');
     return { message: 'Hello World' };
   }
 
@@ -26,6 +29,10 @@ class TestController {
 class TestModule {}
 
 describe('app', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   describe('createApp', () => {
     it('creates a nestjs application', async () => {
       const app = await createTestingApp(TestModule, {
@@ -51,6 +58,23 @@ describe('app', () => {
         message: 'test',
         statusCode: 400,
       });
+    });
+
+    it('logs using the provided logger', async () => {
+      const mockLogger = new MockLogger();
+
+      const logSpy = vi.spyOn(mockLogger, 'log');
+
+      const app = await createTestingApp(TestModule, {
+        logger: mockLogger,
+      });
+
+      vi.clearAllMocks();
+
+      await request(app.getHttpServer()).get('/test');
+
+      expect(logSpy).toHaveBeenCalledOnce();
+      expect(logSpy).toHaveBeenCalledWith('some testing log', TestController.name);
     });
   });
 });
