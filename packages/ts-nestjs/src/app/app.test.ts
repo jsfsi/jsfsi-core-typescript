b6414faaf7731da624ba37fb3a2bea90aec3f596
Joao Franco <joao.franco@jsfsi.com>
2026-01-25 11:08:44 +0000

    Cleanup

--- packages/ts-nestjs/src/app/app.test.ts
+++ packages/ts-nestjs/src/app/app.test.ts
@@ -1,11 +1,10 @@
 import { MockLogger } from '@jsfsi-core/ts-nodejs';
-import { BadRequestException, Body, Controller, Get, Logger, Module, Post } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Logger, Module } from '@nestjs/common';
 import request from 'supertest';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 
 import { appConfigModuleSetup } from '../configuration/AppConfigurationService';
 import { createTestingApp } from '../test/testing-app';
-import { SafeRawBody } from '../validators/ZodValidator';
 
 @Controller('test')
 class TestController {
@@ -15,12 +14,6 @@ class TestController {
   getHello(): { message: string } {
     this.logger.log('some testing log');
     return { message: 'Hello World' };
-  }
-
-  @Post()
-  rawBody(@Body() body: unknown, @SafeRawBody() rawBody: string) {
-    this.logger.debug(rawBody);
-    this.logger.debug(body);
   }
 
   @Get('http-error')
@@ -82,21 +75,6 @@ describe('app', () => {
 
       expect(logSpy).toHaveBeenCalledOnce();
       expect(logSpy).toHaveBeenCalledWith('some testing log', TestController.name);
-    });
-
-    it('has parsed body when json and also the raw body', async () => {
-      const mockLogger = new MockLogger();
-
-      const logSpy = vi.spyOn(mockLogger, 'debug');
-
-      const app = await createTestingApp(TestModule, {
-        logger: mockLogger,
-      });
-
-      await request(app.getHttpServer()).post('/test').send({ foo: 'bar' });
-
-      expect(logSpy).toHaveBeenNthCalledWith(1, '{"foo":"bar"}', TestController.name);
-      expect(logSpy).toHaveBeenNthCalledWith(2, { foo: 'bar' }, TestController.name);
     });
   });
 });
--- packages/ts-nestjs/src/validators/ZodValidator.ts
+++ packages/ts-nestjs/src/validators/ZodValidator.ts
@@ -30,29 +30,3 @@ export function SafeBody<T>(schema: ZodType<T>) {
 export function SafeParams<T>(schema: ZodType<T>) {
   return ZodValidator(schema, 'params');
 }
-
-/* v8 ignore start */
-export function SafeRawBody() {
-  return createParamDecorator((_: unknown, ctx: ExecutionContext): string => {
-    const request = ctx.switchToHttp().getRequest();
-    // In NestJS with rawBody: true, the raw body is stored in req.rawBody as a Buffer
-    // Check multiple possible locations where the raw body might be stored
-    const rawBody = request.rawBody || request.body?.rawBody || request._rawBody;
-
-    if (rawBody === undefined || rawBody === null) {
-      // If raw body is not available, try to reconstruct from the parsed body
-      // This is a fallback for when rawBody option might not be working as expected
-      const parsedBody = request.body;
-      if (parsedBody && typeof parsedBody === 'object') {
-        return JSON.stringify(parsedBody);
-      }
-      return '';
-    }
-
-    if (Buffer.isBuffer(rawBody)) {
-      return rawBody.toString('utf8');
-    }
-
-    return String(rawBody);
-  })();
-}
